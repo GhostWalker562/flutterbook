511a43924d8381473e0b4b9f1f945705f19e35e1
Dalvin Josias Sejour <dalvin.sejour@dutchie.com>
2021-12-02 13:13:24 -0500

    Update lib/src/editor/ui/doc_component.dart
    Co-authored-by: Victor Rojas <Victorrent@users.noreply.github.com>

--- lib/src/editor/ui/doc_component.dart
+++ lib/src/editor/ui/doc_component.dart
@@ -92,8 +92,8 @@ class _DocPanelState extends State<DocPanel> {
                   ),
                   const SizedBox(width: 8),
                   StyledTextButton(
-                    onPressed: zoomOut,
                     icon: FeatherIcons.zoomOut,
+                    onPressed: zoomOut,
                   ),
                   const SizedBox(width: 8),
                   StyledTextButton(
